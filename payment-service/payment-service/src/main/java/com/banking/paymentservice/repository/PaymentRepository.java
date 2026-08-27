f614bc400d399df387715ec8f656cb797a21e202
Rocky Sarkar <sarkarrocky043@gmail.com>
2026-08-27 12:00:43 +0530

    All service completed

--- payment-service/payment-service/src/main/java/com/banking/paymentservice/repository/PaymentRepository.java
+++ payment-service/payment-service/src/main/java/com/banking/paymentservice/repository/PaymentRepository.java
@@ -2,6 +2,12 @@ package com.banking.paymentservice.repository;
 
 import com.banking.paymentservice.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PaymentRepository extends JpaRepository<Payment,String> {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+    Optional<Payment> findByRazorpayOrderId(String orderId);
 }
